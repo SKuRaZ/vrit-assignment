6c80a78180a808c486ac90c9155b051cba43fd16
SKuRaZ <147664162+SKuRaZ@users.noreply.github.com>
2024-02-04 10:46:30 +0545

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,13 @@
+function toggleHeart() {
+    document.querySelector('.heart-2').classList.toggle('clicked');
+  }
+
+
+//   document.addEventListener('DOMContentLoaded', function () {
+//     var menuOpCheckbox = document.querySelector('.menu-op');
+//     var mobileOpenLabel = document.querySelector('.mobile-open');
+
+//     mobileOpenLabel.addEventListener('click', function () {
+//         menuOpCheckbox.checked = !menuOpCheckbox.checked;
+//     });
+// });
